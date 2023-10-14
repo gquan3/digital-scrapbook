705914b4aac3430ec5fed0b088091db1be5b4420
Adrian-Kalisz <adrian.kalisz@outlook.com>
2023-10-14 15:33:29 -0800

    Set the project structure up

---
+++ src/main/java/com/scrapbook/digitalscrapbook/DigitalScrapbookApplication.java
@@ -0,0 +1,13 @@
+package com.scrapbook.digitalscrapbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalScrapbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalScrapbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scrapbook/digitalscrapbook/DigitalScrapbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrapbook.digitalscrapbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalScrapbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
